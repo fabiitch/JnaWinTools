fc6fa753f7466e59616e8db7698afa3879a9a5c5
Fabiitch <fabiitch@users.noreply.github.com>
2026-02-06 00:06:01 +0100

    Convert debug logs to trace and add hook trace logs

--- src/main/java/com/nz/jnawintools/hook/list/WindowVisibilityHook.java
+++ src/main/java/com/nz/jnawintools/hook/list/WindowVisibilityHook.java
@@ -3,8 +3,8 @@ package com.nz.jnawintools.hook.list;
 import com.nz.jnawintools.hook.event.WindowEventAction;
 import com.nz.jnawintools.hook.event.dispatch.AbstractEventDispatcher;
 import com.nz.jnawintools.hook.window.WindowChecker;
-import org.slf4j.Logger;
 import com.sun.jna.platform.win32.WinDef;
+import org.slf4j.Logger;
 
 import static com.nz.jnawintools.hook.cst.WinEventConstants.EVENT_OBJECT_HIDE;
 import static com.nz.jnawintools.hook.cst.WinEventConstants.EVENT_OBJECT_SHOW;
@@ -20,18 +20,29 @@ public class WindowVisibilityHook extends BaseWindowHook {
     protected void onEvent(WinDef.DWORD event, WinDef.HWND hwnd,
                            WinDef.LONG idObject, WinDef.LONG idChild,
                            WinDef.DWORD dwEventThread, WinDef.DWORD dwmsEventTime) {
+        if (idObject.intValue() != 0 || idChild.intValue() != 0) {
+            logger.trace("[{}] ignored event={} (idObject={}, idChild={})", name(), event.intValue(), idObject.intValue(), idChild.intValue());
+            return;
+        }
+
         String windowTitle = window64Helper.getName(hwnd);
-        if (!windowToTrackChecker.isWindow(hwnd)) return;
+        if (!windowToTrackChecker.isWindow(hwnd)) {
+            logger.trace("[{}] ignored event={} for non tracked window={}", name(), event.intValue(), windowTitle);
+            return;
+        }
 
         // Ignore si la fenêtre est minimisée : on laisse MinimiseHook gérer ça
         if (window64Helper.isIconic(hwnd)) {
+            logger.trace("[{}] ignored visibility event={} because window is iconic (hwnd={} title={})", name(), event.intValue(), hwnd, windowTitle);
             return;
         }
         // Déclencher SHOW ou HIDE
         if (event.intValue() == EVENT_OBJECT_SHOW) {
-//                    dispatch(WindowEventAction.Show);
+            logger.trace("[{}] tracked window show event for hwnd={} title={}", name(), hwnd, windowTitle);
+//            dispatch(WindowEventAction.Show);
         } else if (event.intValue() == EVENT_OBJECT_HIDE) {
-//                    dispatch(WindowEventAction.Hide);
+            logger.trace("[{}] tracked window hide event for hwnd={} title={}", name(), hwnd, windowTitle);
+//            dispatch(WindowEventAction.Hide);
         }
     }
 
